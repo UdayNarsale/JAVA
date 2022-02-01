e208fdc6ae0aba105a1415407e6fe0146c03da5f
UdayNarsale <91378332+UdayNarsale@users.noreply.github.com>
2022-02-01 15:10:51 +0530

    Add files via upload

---
+++ Program124.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Marvellous
+{
+	public void Display(int a, int b, int c)
+	{
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
+
+class Program124
+{
+	public static void main(String a[])
+	{
+		Marvellous mobj = new Marvellous();
+		mobj.Display(11,21,51);
+	}
+}
+
+
+
+
+
+
